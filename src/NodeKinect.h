8385030c369aca0537ea714ec0ece7af6520eab7
HaykoKoryun <haykokoryun@gmail.com>
2013-10-24 19:24:49 +0400

    initial commit

---
+++ src/NodeKinect.h
@@ -0,0 +1,130 @@
+﻿//------------------------------------------------------------------------------
+// <copyright file="SkeletonBasics.h" company="Microsoft">
+//     Copyright (c) Microsoft Corporation.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+
+#pragma once
+
+#include "resource.h"
+#include "NuiApi.h"
+#include <node.h>
+#include <v8.h>
+
+typedef struct D2D_VECTOR_3F {
+  FLOAT x;
+  FLOAT y;
+  USHORT z;
+} D2D_VECTOR_3F;
+
+class NodeKinect
+{
+    static const int        cScreenWidth  = 320;
+    static const int        cScreenHeight = 240;
+
+    static const int        cStatusMessageMaxLen = MAX_PATH*2;
+
+public:
+
+    NodeKinect();
+
+    ~NodeKinect();
+
+	//Handle<Value> NodeUpdate(const Arguments& args);
+
+    static LRESULT CALLBACK MessageRouter(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
+
+    /// <summary>
+    /// Handle windows messages for a class instance
+    /// </summary>
+    /// <param name="hWnd">window message is for</param>
+    /// <param name="uMsg">message</param>
+    /// <param name="wParam">message data</param>
+    /// <param name="lParam">additional message data</param>
+    /// <returns>result of message processing</returns>
+    LRESULT CALLBACK        DlgProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
+
+    /// <summary>
+    /// Creates the main window and begins processing
+    /// </summary>
+    /// <param name="hInstance"></param>
+    /// <param name="nCmdShow"></param>
+    int                     Run(HINSTANCE hInstance, int nCmdShow);
+
+	HANDLE                  m_hNextSkeletonEvent;
+
+	void                    Update();
+	
+	D2D_VECTOR_3F            m_Points3D[NUI_SKELETON_POSITION_COUNT];
+private:
+	D2D1_POINT_2F            m_Points[NUI_SKELETON_POSITION_COUNT];
+    HWND                    m_hWnd;
+
+    bool                    m_bSeatedMode;
+
+    // Current Kinect
+    INuiSensor*             m_pNuiSensor;
+
+    // Skeletal drawing
+    ID2D1HwndRenderTarget*   m_pRenderTarget;
+    ID2D1SolidColorBrush*    m_pBrushJointTracked;
+    ID2D1SolidColorBrush*    m_pBrushJointInferred;
+    ID2D1SolidColorBrush*    m_pBrushBoneTracked;
+    ID2D1SolidColorBrush*    m_pBrushBoneInferred;
+	ID2D1SolidColorBrush*    m_pBrushBoneHands;
+    
+
+    // Direct2D
+    ID2D1Factory*           m_pD2DFactory;
+    
+    HANDLE                  m_pSkeletonStreamHandle;    
+
+    /// <summary>
+    /// Create the first connected Kinect found 
+    /// </summary>
+    /// <returns>S_OK on success, otherwise failure code</returns>
+    HRESULT                 CreateFirstConnected();
+
+    /// <summary>
+    /// Handle new skeleton data
+    /// </summary>
+    void                    ProcessSkeleton();
+
+    /// <summary>
+    /// Ensure necessary Direct2d resources are created
+    /// </summary>
+    /// <returns>S_OK if successful, otherwise an error code</returns>
+    HRESULT                 EnsureDirect2DResources( );
+
+    /// <summary>
+    /// Dispose Direct2d resources 
+    /// </summary>
+    void                    DiscardDirect2DResources( );
+
+    /// <summary>
+    /// Draws a bone line between two joints
+    /// </summary>
+    /// <param name="skel">skeleton to draw bones from</param>
+    /// <param name="joint0">joint to start drawing from</param>
+    /// <param name="joint1">joint to end drawing at</param>
+    void                    DrawBone(const NUI_SKELETON_DATA & skel, NUI_SKELETON_POSITION_INDEX bone0, NUI_SKELETON_POSITION_INDEX bone1);
+
+    /// <summary>
+    /// Draws a skeleton
+    /// </summary>
+    /// <param name="skel">skeleton to draw</param>
+    /// <param name="windowWidth">width (in pixels) of output buffer</param>
+    /// <param name="windowHeight">height (in pixels) of output buffer</param>
+    void                    DrawSkeleton(const NUI_SKELETON_DATA & skel, int windowWidth, int windowHeight);
+
+    D2D1_POINT_2F           SkeletonToScreen(Vector4 skeletonPoint, int width, int height);
+
+	D2D_VECTOR_3F			SkeletonToScreen3D(Vector4 skeletonPoint, int width, int height);
+
+
+    /// <summary>
+    /// Set the status bar message
+    /// </summary>
+    /// <param name="szMessage">message to display</param>
+    void                    SetStatusMessage(WCHAR* szMessage);
+};
---
+++ src/binding.gyp
@@ -0,0 +1,19 @@
+﻿{
+  "targets": [
+    {
+      "target_name": "Kinect",
+      "sources": [ "NodeKinect.cpp" ],
+      "include_dirs":
+      [
+        "C:\\Program Files\\Microsoft SDKs\\Kinect\\v1.7\\inc",
+        "C:\\Program Files\\Microsoft SDKs\\Windows\\v7.1\\Include",
+        ".\\"
+      ],
+      'libraries':
+      [
+        "C:\\Program Files\\Microsoft SDKs\\Kinect\\v1.7\\lib\\amd64\\Kinect10.lib",
+        "C:\\Program Files\\Microsoft SDKs\\Windows\\v7.1\\Lib\\x64\\User32.lib",
+      ]
+    }
+  ]
+}
